1d28eda0487117581b96630888f44ae06164e2f6
Jesseh78 <joseulises59@gmail.com>
2025-06-18 10:41:20 -0300

    atualização de exemplos

--- EstruturasRepeticao/for.py
+++ EstruturasRepeticao/for.py
@@ -5,3 +5,5 @@ vogais = "AEIOU"
 for letra in texto:
     if letra.upper() in vogais:
         print(letra, end = "")
+
+# nesse exemplo o FOR faz com que a string seja percorrida printando somente as vogais 
